d691897e1e81c2651dc9cd52f10169e35a21e33f
jaekeun Choi <jaek1783@gmail.com>
2022-06-12 20:27:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Logo from './img/logo/logo.png';
 
-function App() {
+const App=()=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className='header'>
+          <h1 className='logo'><img src={Logo} alt="로고이미지" /></h1>
+          <div className='search'>
+            <div className='allSearch'>통합검색</div>
+              <input type="text" placeholder='자격증 꿀팁 보러가기 #취준 #취뽀' maxLength='50'/>
+          </div>
+          <div className='login_container'>
+            <ul className='login'>
+              <li>회원가입</li>
+              <li>로그인</li>
+              <li>가입서비스</li>
+            </ul>
+          </div>
+        </div>
+        <div className="nav"></div>
+        <div className='subNav'></div>
+      </div>
     </div>
   );
 }
