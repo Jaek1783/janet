3ed76eaf8810b0a96320515c8fc5efdb216f941c
jaekeun Choi <jaek1783@gmail.com>
2022-06-20 09:11:26 +0900

    사진수정

--- src/App.js
+++ src/App.js
@@ -100,56 +100,56 @@ const App=()=> {
       "id":1,
       "title":"토익(TOEIC)",
       "licenseOrgan":"미국 ETS",
-      "esRegdt":"2021-06-11",
+      "esRegdt":"2022-06-11",
       "img":keyword01
     },
     {
       "id":2,
       "title":"한국사능력검정시험",
       "licenseOrgan":"국사편찬위원회",
-      "esRegdt":"2021-07-13",
+      "esRegdt":"2022-07-13",
       "img":keyword02
     },
     {
       "id":3,
       "title":"전기기사",
       "licenseOrgan":"한국산업인력공단",
-      "esRegdt":"2021-07-13",
+      "esRegdt":"2022-07-13",
       "img":keyword03
     },
     {
       "id":4,
       "title":"공인중개사",
       "licenseOrgan":"한국산업인력공단",
-      "esRegdt":"2021-07-13",
+      "esRegdt":"2022-07-13",
       "img":keyword04
     },
     {
       "id":5,
       "title":"산업안전기사",
       "licenseOrgan":"한국산업인력공단",
-      "esRegdt":"2021-07-13",
+      "esRegdt":"2022-07-13",
       "img":keyword05
     },
     {
       "id":6,
       "title":"KBS한국어능력시험",
       "licenseOrgan":"KBS 한국어진흥원",
-      "esRegdt":"2021-07-02",
+      "esRegdt":"2022-07-02",
       "img":keyword06
     },
     {
       "id":7,
       "title":"요양보호사",
       "licenseOrgan":"한국보건의료인국가시험원",
-      "esRegdt":"2021-08-06",
+      "esRegdt":"2022-08-06",
       "img":keyword07
     },
     {
       "id":8,
       "title":"ITQ",
       "licenseOrgan":"한국생산성본부",
-      "esRegdt":"2021-07-13",
+      "esRegdt":"2022-07-13",
       "img":keyword08
     }
   ];
